b459bdc80d3c4a3685cea50fa45ce225900926c9
Ovan Crone <ovan@psibr.net>
2016-03-24 11:34:15 -0500

    Initial code checking

---
+++ src/JwtAndCookie/JwtAndCookieMiddlewareOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace OwinJwtAndCookie
+{
+    public class JwtAndCookieMiddlewareOptions
+    {
+        public JwtAndCookieMiddlewareOptions()
+        {
+            CookieHttpOnly = true;
+        }
+
+        public string PassPhrase { get; set; }
+
+        public string CookieName { get; set; }
+
+        public string CookiePath { get; set; }
+
+        public bool CookieHttpOnly { get; set; }
+
+        public string ClaimsPrincipalResourceName { get; set; }
+
+        public TimeSpan TokenLifeSpan { get; set; } = TimeSpan.FromMinutes(30);
+
+        public Func<IDictionary<string, object>, ClaimsPrincipal> CreatePrincipal { get; set; }
+    }
+}
